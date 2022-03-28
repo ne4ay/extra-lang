e960714391eaf4cbf68729dfd62a9eacf95fd101
Aleksei Nechaev <anechaev@devexperts.com>
2022-03-29 00:56:03 +0300

    Initial commit

---
+++ src/main/java/ua/nechay/javaextras/Either.java
@@ -0,0 +1,140 @@
+package ua.nechay.javaextras;
+
+import javax.annotation.Nonnull;
+import java.util.NoSuchElementException;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+/**
+ * Represents a disjoint union, containing either something or something else.
+ *
+ * @param <LEFT_TYPE>  - type of the left value
+ * @param <RIGHT_TYPE> - type of the right value
+ * @author anechaev
+ * @since 28.03.2022
+ */
+public abstract class Either<LEFT_TYPE, RIGHT_TYPE> {
+
+    /**
+     * @return the left value or throws an exception if this is right
+     * @throws NoSuchElementException if this is right value
+     */
+    public LEFT_TYPE getLeft() {
+        throw new NoSuchElementException("No left element present");
+    }
+
+    public RIGHT_TYPE getRight() {
+        throw new NoSuchElementException("No right element present");
+    }
+
+    /**
+     * @return true if it is left value, false otherwise
+     */
+    public abstract boolean isLeft();
+
+    /**
+     * @return true if it is right value, false otherwise
+     */
+    public abstract boolean isRight();
+
+    public abstract Either<LEFT_TYPE, RIGHT_TYPE> onLeft(@Nonnull Consumer<LEFT_TYPE> leftConsumer);
+
+    public abstract Either<LEFT_TYPE, RIGHT_TYPE> onRight(@Nonnull Consumer<RIGHT_TYPE> rightConsumer);
+
+    public abstract <RESULT> Either<RESULT, RIGHT_TYPE> mapLeft(@Nonnull Function<LEFT_TYPE, RESULT> leftFunction);
+
+    public abstract <RESULT> Either<LEFT_TYPE, RESULT> mapRight(@Nonnull Function<RIGHT_TYPE, RESULT> rightFunction);
+
+    public Either<RIGHT_TYPE, LEFT_TYPE> swap() {
+        return isRight() ? Either.left(getRight()) : Either.right(getLeft());
+    }
+
+    public static <LEFT_TYPE, RIGHT_TYPE> Either<LEFT_TYPE, RIGHT_TYPE> left(LEFT_TYPE left) {
+        return null;
+    }
+
+    public static <LEFT_TYPE, RIGHT_TYPE> Either<LEFT_TYPE, RIGHT_TYPE> right(RIGHT_TYPE right) {
+        return null;
+    }
+
+    private static class Left<LEFT_TYPE, RIGHT_TYPE> extends Either<LEFT_TYPE, RIGHT_TYPE> {
+
+        private final LEFT_TYPE left;
+
+        private Left(@Nonnull LEFT_TYPE left) {
+            this.left = left;
+        }
+
+        @Override
+        public boolean isLeft() {
+            return true;
+        }
+
+        @Override
+        public boolean isRight() {
+            return false;
+        }
+
+        @Override
+        public Either<LEFT_TYPE, RIGHT_TYPE> onLeft(@Nonnull Consumer<LEFT_TYPE> leftConsumer) {
+            leftConsumer.accept(left);
+            return this;
+        }
+
+        @Override
+        public Either<LEFT_TYPE, RIGHT_TYPE> onRight(@Nonnull Consumer<RIGHT_TYPE> rightConsumer) {
+            return this;
+        }
+
+        @Override
+        public <RESULT> Either<RESULT, RIGHT_TYPE> mapLeft(@Nonnull Function<LEFT_TYPE, RESULT> leftFunction) {
+            return Either.left(leftFunction.apply(left));
+        }
+
+        @Override
+        public <RESULT> Either<LEFT_TYPE, RESULT> mapRight(@Nonnull Function<RIGHT_TYPE, RESULT> rightFunction) {
+            return Either.left(left);
+        }
+    }
+
+    private static class Right<LEFT_TYPE, RIGHT_TYPE> extends Either<LEFT_TYPE, RIGHT_TYPE> {
+
+        private final RIGHT_TYPE right;
+
+        private Right(@Nonnull RIGHT_TYPE right) {
+            this.right = right;
+        }
+
+        @Override
+        public boolean isLeft() {
+            return false;
+        }
+
+        @Override
+        public boolean isRight() {
+            return true;
+        }
+
+        @Override
+        public Either<LEFT_TYPE, RIGHT_TYPE> onLeft(@Nonnull Consumer<LEFT_TYPE> leftConsumer) {
+            return this;
+        }
+
+        @Override
+        public Either<LEFT_TYPE, RIGHT_TYPE> onRight(@Nonnull Consumer<RIGHT_TYPE> rightConsumer) {
+            rightConsumer.accept(right);
+            return this;
+        }
+
+        @Override
+        public <RESULT> Either<RESULT, RIGHT_TYPE> mapLeft(@Nonnull Function<LEFT_TYPE, RESULT> leftFunction) {
+            return Either.right(right);
+        }
+
+        @Override
+        public <RESULT> Either<LEFT_TYPE, RESULT> mapRight(@Nonnull Function<RIGHT_TYPE, RESULT> rightFunction) {
+            return Either.right(rightFunction.apply(right));
+        }
+    }
+
+}
---
+++ src/main/java/ua/nechay/javaextras/Validation.java
@@ -0,0 +1,72 @@
+package ua.nechay.javaextras;
+
+import javax.annotation.Nonnull;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+/**
+ * @author anechaev
+ * @since 28.03.2022
+ */
+public class Validation<EXCEPTION_TYPE extends Throwable, RESULT_TYPE> extends Either<EXCEPTION_TYPE, RESULT_TYPE> {
+
+    private final Either<EXCEPTION_TYPE, RESULT_TYPE> value;
+
+    private Validation(Either<EXCEPTION_TYPE, RESULT_TYPE> either) {
+        this.value = either;
+    }
+
+    public static <ORIGINAL_TYPE, RESULT_TYPE> Validation<Throwable, RESULT_TYPE>
+    tryOf(@Nonnull ORIGINAL_TYPE arg, @Nonnull Function<ORIGINAL_TYPE, RESULT_TYPE> function) {
+        try {
+            return new Validation<>(Either.right(function.apply(arg)));
+        } catch (Throwable t) {
+            return new Validation<>(Either.left(t));
+        }
+    }
+
+    public <MAYBE_EXCEPTION_TYPE extends Throwable> Validation<EXCEPTION_TYPE, RESULT_TYPE>
+    onCatch(@Nonnull Class<MAYBE_EXCEPTION_TYPE> exceptionType, @Nonnull Consumer<EXCEPTION_TYPE> consumer) {
+        if (value.isLeft() && (exceptionType.isInstance(value.getLeft()))) {
+            consumer.accept(value.getLeft());
+        }
+        return this;
+    }
+
+    public Validation<EXCEPTION_TYPE, RESULT_TYPE> raise() throws Throwable {
+        if (value.isLeft()) {
+            throw value.getLeft();
+        }
+        return this;
+    }
+
+    @Override
+    public boolean isLeft() {
+        return value.isLeft();
+    }
+
+    @Override
+    public boolean isRight() {
+        return value.isRight();
+    }
+
+    @Override
+    public Either<EXCEPTION_TYPE, RESULT_TYPE> onLeft(@Nonnull Consumer<EXCEPTION_TYPE> leftConsumer) {
+        return value.onLeft(leftConsumer);
+    }
+
+    @Override
+    public Either<EXCEPTION_TYPE, RESULT_TYPE> onRight(@Nonnull Consumer<RESULT_TYPE> rightConsumer) {
+        return value.onRight(rightConsumer);
+    }
+
+    @Override
+    public <RESULT> Either<RESULT, RESULT_TYPE> mapLeft(@Nonnull Function<EXCEPTION_TYPE, RESULT> leftFunction) {
+        return value.mapLeft(leftFunction);
+    }
+
+    @Override
+    public <RESULT> Either<EXCEPTION_TYPE, RESULT> mapRight(@Nonnull Function<RESULT_TYPE, RESULT> rightFunction) {
+        return value.mapRight(rightFunction);
+    }
+}
